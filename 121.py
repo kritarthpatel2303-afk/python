4ae3cb0efe1d08bebce429264776b7daebef3650
Kritarth Patel <kritarthpatel2303@gmail.com>
2026-01-21 17:42:41 +0530

    Add files via upload

---
+++ 121.py
@@ -0,0 +1,10 @@
+a=int(input("enter length : "))
+b=int(input("enter breath : "))
+if(a*b>(2*(a+b))):
+    print("area is greater than perimeter")
+else:
+    print("perimeter is greater than area")
+    
+
+
+
