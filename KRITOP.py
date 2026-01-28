88c09b735f8d99d07b230bbe8292303c12d8f0d1
Kritarth Patel <kritarthpatel2303@gmail.com>
2026-01-28 16:59:17 +0530

    Add files via upload

---
+++ KRITOP.py
@@ -0,0 +1,6 @@
+str=input("enter a string:")
+count=0
+for a in str:
+    if(a=='a' or a=='e' or a=='i' or a== 'o' or a=='u' or a=='A' or a== 'E' or a=='I' or a=='O' or a=='U' ):
+        count+=1
+print(count)
