bba539096b9bb11f30cfa3d194b426bc2245c705
Kritarth Patel <kritarthpatel2303@gmail.com>
2026-02-04 17:20:28 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-for i in range(1,21):
-    if i%2 ==0:
-        print(i)
-        
+n=int(input("enter number of elements"))
+a=0
+b=1
+print("fibbonacci series is :")
+for i in range (n):
+    print(a,end=" ")
+    c=a+b
+    a=b
+    b=c
